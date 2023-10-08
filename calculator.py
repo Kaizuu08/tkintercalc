dfd83df81cf4c47ea72ac6578eda599390e845d0
Duc <ducnguyen81103@gmail.com>
2023-10-08 10:36:04 +1000

    basic structure of calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+## store calculator calculations
+calculation = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def evaluate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+root = tk.Tk()
+root.geometry("300x300")
+root.mainloop()
