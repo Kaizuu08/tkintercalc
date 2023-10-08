8411a748a9ca75ed0463348af7f10f3f99b08426
Duc <ducnguyen81103@gmail.com>
2023-10-08 10:37:20 +1000

    text box and grid space

--- calculator.py
+++ calculator.py
@@ -13,4 +13,10 @@ def clear_field():
 
 root = tk.Tk()
 root.geometry("300x300")
+
+## text box and grid space
+text_result = tk.Text(root, height = 2, width =16, font=("Arial",24))
+text_result.grid(columnspan = 5)
+
+#runs the program window
 root.mainloop()
