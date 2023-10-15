51e1fa87603c10be2d4a7ee20ce9f7d63351d879
Duc <ducnguyen81103@gmail.com>
2023-10-15 16:57:12 +1000

    improved

--- calculator.py
+++ calculator.py
@@ -28,6 +28,26 @@ def clear_field():
     calculation = ""
     text_result.delete(1.0, "end")
     
+## function to togle display
+def toggle_mode():
+    current_bg = root.cget("background")
+    if current_bg == "black":
+        # Switch to light mode
+        root.configure(background="white")
+        set_text_color("black")
+        text_result.configure(bg="white", fg="black")
+    else:
+        # Switch to night mode
+        root.configure(background="black")
+        set_text_color("white")
+        text_result.configure(bg="black", fg="white")
+
+## function to change text colour
+def set_text_color(color):
+    for widget in root.winfo_children():
+        if isinstance(widget, (tk.Button, tk.Text)):
+            widget.configure(foreground=color)
+    
 
 root = tk.Tk()
 root.geometry("300x300")
@@ -74,10 +94,29 @@ btn_minus = tk.Button(root, text="-", command=lambda: add_to_calculation("-"), w
 btn_minus.grid(row=3, column=4)
 
 btn_equals = tk.Button(root, text="=", command=evaluate_calculation, width=5, font=("Arial", 14))
-btn_equals.grid(row=4, column=4)
+btn_equals.grid(row=6, column=4)
 
-btn_clear = tk.Button(root, text="C", command=clear_field, width=5, font=("Arial", 14)) #lambda is not needed as we are passing a function
+btn_clear = tk.Button(root, text="C", command=clear_field, width=5, font=("Arial", 14)) 
 btn_clear.grid(row=5, column=1)
+
+btn_multiply = tk.Button(root, text="*", command=lambda: add_to_calculation("*"), width=5, font=("Arial", 14))
+btn_multiply.grid(row=4, column=4)
+
+btn_divide = tk.Button(root, text="/", command=lambda: add_to_calculation("/"), width=5, font=("Arial", 14)) 
+btn_divide.grid(row=5, column=4)
+
+btn_bracket1 = tk.Button(root, text="(", command=lambda: add_to_calculation("("), width=5, font=("Arial", 14))
+btn_bracket1.grid(row=6, column=1)
+
+btn_bracket2 = tk.Button(root, text=")", command=lambda: add_to_calculation(")"), width=5, font=("Arial", 14)) 
+btn_bracket2.grid(row=6, column=2)
+
+btn_night_mode = tk.Button(root, text="Night", command=toggle_mode, width=5, font=("Arial", 14))
+btn_night_mode.grid(row=5, column=3)
+
+btn_light_mode = tk.Button(root, text="Light", command=toggle_mode, width=5, font=("Arial", 14))
+btn_light_mode.grid(row=6, column=3)
+
 
 ## runs the program window
 root.mainloop()
